ea37777efa3d380c0b5db949b2ecc1e123dbe8c1
madhuvayya <madhu.vayya@gmail.com>
2020-04-18 12:42:10 +0530

    Added function to find who plays first

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -1,25 +1,49 @@
 #!/bin/bash -x
 
 NUM_OF_POS=9
+HEAD=1
+TAIL=0
 
 player1=""
 player2=""
 
 declare -A board
 
-for((i=0;i<$NUM_OF_POS;i++))
-do
-	board[$i]="-"
-done
+resetBoard(){
+	for((i=0;i<$NUM_OF_POS;i++))
+	do
+		board[$i]="-"
+	done
+}
 
-player1="o"
+findLetterAssigned(){
+	echo enter plyer1 choice:
+	read player1
 
-if [ $player1 = x ]
-then
-	player2="o"
-else
-	player2="x"
-fi
+	if [ $player1 = x ]
+	then
+		player2="o"
+	else
+		player2="x"
+	fi
 
-echo letter assigned: $player2
+	echo letter assigned to player2: $player2
+}
 
+whoPlaysFirst(){
+
+	rand=$((RANDOM%2))
+
+	if [ $rand -eq $HEAD ]
+	then
+		echo player1
+	else
+		echo player2
+	fi
+}
+
+resetBoard
+
+findLetterAssigned
+
+whoPlaysFirst
