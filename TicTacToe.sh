88ef411a2bce8b4fc240439ba33dec4bd85eebfc
madhuvayya <madhu.vayya@gmail.com>
2020-04-18 12:11:26 +0530

    Added if statement to know the assigned letter

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -1,10 +1,25 @@
 #!/bin/bash -x
 
+NUM_OF_POS=9
+
+player1=""
+player2=""
+
 declare -A board
 
-for((i=0;i<9;i++))
+for((i=0;i<$NUM_OF_POS;i++))
 do
-	board[$i]=-
+	board[$i]="-"
 done
 
-echo ${board[@]}
+player1="o"
+
+if [ $player1 = x ]
+then
+	player2="o"
+else
+	player2="x"
+fi
+
+echo letter assigned: $player2
+
