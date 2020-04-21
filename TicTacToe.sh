b2404fcd4f85e208e9da7ce566ba3f9ed6ee1354
madhuvayya <madhu.vayya@gmail.com>
2020-04-21 11:16:09 +0530

    Added code for computer's play

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -6,10 +6,13 @@ NUM_OF_POS=$(($NUM_OF_ROWS*$NUM_OF_COLMS))
 HEAD=1
 TAIL=0
 
-player1=""
-player2=""
+computerLtr=""
+yourLtr=""
 
-numOfTurns=0
+turn=""
+
+position=0
+numOfTurns=1
 
 declare -A board
 
@@ -21,29 +24,28 @@ resetBoard(){
 }
 
 findLetterAssigned(){
-
-	echo enter plyer1 choice:
-	read player1
-
-	if [ $player1 = x ]
+	rand=$((RANDOM%2))
+	if [ $rand = 0 ]
 	then
-		player2="o"
+		computerLtr="o"
+		yourLtr="x"
 	else
-		player2="x"
+		computerLtr="x"
+		yourLtr="o"
 	fi
-
-	echo letter assigned to player2: $player2
+	echo Computer letter: $computerLtr
+	echo Letter assigned to you: $yourLtr
 }
 
 whoPlaysFirst(){
-
 	rand=$((RANDOM%2))
-
 	if [ $rand -eq $HEAD ]
 	then
-		echo player1 won toss
+		echo computer won toss
+		turn="computer"
 	else
-		echo player2 won toss
+		echo you won toss
+		turn="your"
 	fi
 }
 
@@ -64,6 +66,7 @@ checkRows(){
 		t=$((3*$i))
 		if [[ ${board[$((1+$t))]} != "-" &&  ${board[$((1+$t))]} = ${board[$((2+$t))]} && ${board[$((2+$t))]} =  ${board[$((3+$t))]} ]]
 		then
+			echo ${board[$((1+$t))]}
 			winner ${board[$((1+$t))]}
 		fi
 	done
@@ -92,12 +95,11 @@ checkDiag(){
 
 winner(){
 
-	if [ $1 = $player1 ]
+	if [ $1 = $computerLtr ]
 	then
-		player=$player1
-	elif [ $1 = $player2 ]
-	then
-		player=$player2
+		player="computer"
+	else
+		player="you"
 	fi
 
 	printBoard
@@ -106,20 +108,20 @@ winner(){
 }
 
 checkTie(){
-	temp=0
-	for((i=1;i<=$NUM_OF_POS;i++))
-	do
-		if [ ${board[$i]} != "-" ]
-		then
-			((temp++))
-		fi
-	done
 
-	if [ $temp -eq $NUM_OF_POS ]
+	if [ $numOfTurns -eq $NUM_OF_POS ]
 	then
 		echo game is tie
+		exit
 	else
-		echo changeTurn
+		if [ $turn = computer ]
+		then
+			turn="your"
+		else
+			turn="computer"
+		fi
+
+		play
 	fi
 }
 
@@ -134,10 +136,151 @@ checkWinnerTie(){
 	checkTie
 }
 
-resetBoard
+rowWinningMove(){
+	for((i=0;i<$NUM_OF_ROWS;i++))
+	do
+		t=$((3*$i))
 
-findLetterAssigned
+		a=${board[$((1+$t))]}
+		b=${board[$((2+$t))]}
+		c=${board[$((3+$t))]}
 
-whoPlaysFirst
+		if [[ $a = $1 && $b = $1 && $c = "-" ]]
+		then
+			position=$((3+$t))
+			echo $position
+			break
+		elif [[ $b = $1 && $c = $1 && $a = "-" ]]
+		then
+			position=$((1+$t))
+			echo $position
+			break
+		elif [[ $a = $1 && $c = $1 && $b = "-" ]]
+		then
+			position=$((2+$t))
+			echo $position
+			break
+		fi
+	done
+}
 
-checkWinnerTie
+colmWinningMove(){
+	for((i=0;i<$NUM_OF_COLMS;i++))
+	do
+		a=${board[$((1+$i))]}
+		b=${board[$((4+$i))]}
+		c=${board[$((7+$i))]}
+
+		if [[ $a = $1 && $b = $1 && $c = "-" ]]
+		then
+			position=$((7+$i))
+			echo $position
+			break
+		elif [[ $b = $1 && $c = $1 && $a = "-" ]]
+		then
+			position=$((1+$i))
+			echo $position
+			break
+		elif [[ $a = $1 && $c = $1 && $b = "-" ]]
+		then
+			position=$((4+$i))
+			echo $position
+			break
+		fi
+	done
+}
+
+diagWinningMove(){
+	for((i=0;i<2;i++))
+	do
+		t=$((2*$i))
+		a=${board[$((1+$t))]}
+		b=${board[$((5))]}
+		c=${board[$((9-$t))]}
+
+		if [[ $a = $1 && $b = $1 && $c = "-" ]]
+		then
+			position=$((9-$t))
+			echo $position
+			break
+		elif [[ $b = $1 && $c = $1 && $a = "-" ]]
+		then
+			position=$((1+$t))
+			echo $position
+			break
+		elif [[ $a = $1 && $c = $1 && $b = "-" ]]
+		then
+			position=5
+			echo $position
+			break
+		fi
+	done
+}
+
+WinningMove(){
+	rowWinningMove $1
+	if [ $position -eq 0 ]
+	then
+		colmWinningMove $1
+		if [ $position -eq 0 ]
+		then
+			diagWinningMove $1
+		fi
+	fi
+}
+
+computerTurn(){
+	WinningMove $ComputerLtr
+}
+
+checkValidPosition(){
+	if [  ${board[$1]} != "-" ]
+	then
+		echo "entered position is already occupied"
+		play
+	fi
+}
+
+initilize(){
+	resetBoard
+
+	findLetterAssigned
+
+	whoPlaysFirst
+}
+
+play(){
+	while [ $numOfTurns -le $NUM_OF_POS  ]
+	do
+		if [ $turn = computer ]
+		then
+			echo computer turn
+			computerTurn
+			board[$position]=$computerLtr
+			checkValidPosition $position
+			((numOfTurns++))
+			printBoard
+			position=0
+			turn="your"
+		else
+			echo It is you turn
+			echo "enter your position(1-9)"
+			read position
+			checkValidPosition $position
+			board[$position]=$yourLtr
+			((numOfTurns++))
+			printBoard
+			checkWinnerTie
+			position=0
+		fi
+
+	done
+}
+
+start(){
+
+	initilize
+	play
+}
+
+start
